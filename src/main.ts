46a0f64f04b1fc0ccfe7fbbbf1a560ad779cbe26
slashleyonline <dseward@ucsc.edu>
2025-10-10 22:43:35 +0000

    added a div that displays the "total" variable.

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,19 @@ button.style.fontSize = "4em";
 button.innerText = "Add";
 document.body.appendChild(button);
 
+const counterDiv = document.createElement("div");
+counterDiv.textContent = "total amount: 0";
+document.body.appendChild(counterDiv);
+
 let total = 0;
 
 button.addEventListener("click", () => {
   console.log("1 person added!");
+  console.log("total amount: ", String(total));
   total += 1;
+  updateCounter(total);
 });
+
+function updateCounter(input: number) {
+  counterDiv.innerText = String("total amount: ") + input;
+}
