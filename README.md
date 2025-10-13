528a27ccbc1ae969657ca5a225debfb56bbbddef
slashleyonline <dseward@ucsc.edu>
2025-10-13 13:23:22 +0000

    Changed readme, added more upgrade buttons, unique div for buttons, unique functions for upgrades, decimals for the displayed growth rate. Added more if/else statements for checkPrices.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CMPM 121 D1 Project
 
-This is the starting point for your CMPM 121 D1 project. The goal of this project is to create a simple web application using TypeScript, Vite, and modern web development practices. Students should make sure to remove _this_ content from their README.md file and replace it with their own project documentation.
+Here's my clicker game project!
+
+The basic theme for this game is summoning people to walk across the screen.
+I'm not fully sure why, I just wanted to have a sort of animated component to this game, 
+and this seemed like it was within scope.
--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,33 @@ button.style.fontSize = "4em";
 button.innerText = "Add";
 gameContainer.appendChild(button);
 
-const buyButton = document.createElement("button");
-buyButton.disabled = true;
-buyButton.style.fontSize = "4em";
-buyButton.innerText = "Buy an autoclicker!";
-gameContainer.appendChild(buyButton);
+const buttonsDiv = document.createElement("div");
+gameContainer.appendChild(buttonsDiv);
+
+const buyButtonA = document.createElement("button");
+buyButtonA.disabled = true;
+buyButtonA.style.fontSize = "4em";
+buyButtonA.innerText = "Buy an autoclicker! - A";
+buttonsDiv.appendChild(buyButtonA);
+
+const buyButtonB = document.createElement("button");
+buyButtonB.disabled = true;
+buyButtonB.style.fontSize = "4em";
+buyButtonB.innerText = "Buy an autoclicker! - B";
+buttonsDiv.appendChild(buyButtonB);
+
+const buyButtonC = document.createElement("button");
+buyButtonC.disabled = true;
+buyButtonC.style.fontSize = "4em";
+buyButtonC.innerText = "Buy an autoclicker! - C";
+buttonsDiv.appendChild(buyButtonC);
 
 const counterDiv = document.createElement("div");
 counterDiv.textContent = "🚶 total amount: 0";
 gameContainer.appendChild(counterDiv);
 
 const growthRateDiv = document.createElement("div");
-counterDiv.textContent = "0 people per second.";
+growthRateDiv.textContent = "0 people per second.";
 gameContainer.appendChild(growthRateDiv);
 
 //VARIABLES RELEVANT TO GAMEPLAY
@@ -39,8 +54,16 @@ button.addEventListener("click", () => {
   buttonClick();
 });
 
-buyButton.addEventListener("click", () => {
-  purchaseClicker();
+buyButtonA.addEventListener("click", () => {
+  purchaseClickerTypeA();
+});
+
+buyButtonB.addEventListener("click", () => {
+  purchaseClickerTypeB();
+});
+
+buyButtonC.addEventListener("click", () => {
+  purchaseClickerTypeC();
 });
 
 //ANIMATION
@@ -78,9 +101,25 @@ function updateCounter(input: number) {
     (Math.trunc(input * 100) / 100);
 }
 
-function purchaseClicker() {
-  autoClickRate += 0.001;
+function purchaseClickerTypeA() {
+  autoClickRate += 0.0001;
   total -= 10;
+
+  checkPrices();
+  updateGrowthRateDiv();
+}
+
+function purchaseClickerTypeB() {
+  autoClickRate += 0.002;
+  total -= 100;
+
+  checkPrices();
+  updateGrowthRateDiv();
+}
+
+function purchaseClickerTypeC() {
+  autoClickRate += 0.05;
+  total -= 1000;
 
   checkPrices();
   updateGrowthRateDiv();
@@ -88,14 +127,26 @@ function purchaseClicker() {
 
 function updateGrowthRateDiv() {
   const rate: number = autoClickRate * 1000;
-  growthRateDiv.innerText = Math.trunc(rate) + String(" people per second.");
+  growthRateDiv.innerText = (Math.trunc(rate * 100) / 100) + String(" people per second.");
 }
 
 function checkPrices() {
   //checks if the prices for any item in the shop are within the player's budget.
   if (total >= 10) {
-    buyButton.disabled = false;
+    buyButtonA.disabled = false;
   } else {
-    buyButton.disabled = true;
+    buyButtonA.disabled = true;
+  }
+
+  if (total >= 100) {
+    buyButtonB.disabled = false;
+  } else {
+    buyButtonB.disabled = true;
+  }
+
+  if (total >= 1000) {
+    buyButtonC.disabled = false;
+  } else {
+    buyButtonC.disabled = true;
   }
 }
